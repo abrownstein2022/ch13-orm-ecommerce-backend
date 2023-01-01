26cd7ebcbb45f9b506ab9e78a8f710b49c647a45
Alexis Brownstein <alexis@drdatabase.com>
2023-01-01 18:44:41 -0500

    completed sequelize setup for ProductTag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,28 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This references the `product` model's id, which was set in Product.js as its `modelName` property
+        model: 'product',
+        key: 'id',
+      },
+    },       
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This references the `tag` model's id, which was set in Tag.js as its `modelName` property
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
