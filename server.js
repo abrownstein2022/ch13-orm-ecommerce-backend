c9174a31988cd03be224e47a7a580af035518657
Alexis Brownstein <alexis@drdatabase.com>
2023-01-01 22:44:03 -0500

    added sync code to server.js

--- server.js
+++ server.js
@@ -11,6 +11,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
+
+// //turn on connection to db and server
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'),PORT);
+// });
+// catch-all route
+// any route that doesn't match any of the defined routes
+app.all('*', (req, res) => {
+  console.log('No route found at:', req.path)
+  res.render(`errorpage`)
+})
+
+//added PORT below so other users will know if the port has changed
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening',PORT));
 });
